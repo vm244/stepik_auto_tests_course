3bb4ea02aef6996ecd8bc6ade663d76f3e57e7d0
vm244 <vmartynenko023@gmail.com>
2026-06-26 23:05:38 +0200

    add login page and page transitions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,26 @@
-from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
-class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
