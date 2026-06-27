a482d3cfaaad4e595c6c9e83b2af98cd5cbc0a7c
vm244 <vmartynenko023@gmail.com>
2026-06-27 22:58:41 +0200

    make product page test independent from product data

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,12 +7,18 @@ class ProductPage(BasePage):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
 
-    def should_be_product_added_to_basket(self):
+    def get_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        return product_name
+
+    def get_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        return product_price
+
+    def should_be_product_added_to_basket(self, product_name):
         success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
         assert product_name == success_message, "Product name in message does not match product name on page"
 
-    def should_be_basket_price_equal_to_product_price(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+    def should_be_basket_price_equal_to_product_price(self, product_price):
         basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
         assert product_price == basket_total, "Basket total does not match product price"
--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,16 @@
-### test_product_page.py
-
 from pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
+
+    product_name = page.get_product_name()
+    product_price = page.get_product_price()
+
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_product_added_to_basket()
-    page.should_be_basket_price_equal_to_product_price()
+
+    page.should_be_product_added_to_basket(product_name)
+    page.should_be_basket_price_equal_to_product_price(product_price)
